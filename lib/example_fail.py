6324fe294fb027c360f83eeafa36c3f2a4b2af6c
João Reis <joao.carlos.reis@cgd.pt>
2023-03-28 13:39:05 +0100

    added failing example c3 linearization

--- lib/example_fail.py
+++ lib/example_fail.py
@@ -7,3 +7,8 @@ class B1(A): pass
 
 class C(A,B1): pass
 
+#L(A) = [A]
+# L(B1) = B1 + merge(L(A) + L(A)) = [B1] + [A] = [B1, A]
+# L(C) = C + merge(L(A) + L(B1) + [A,B1]) = C + ([A] + [B1, A] + [A, B1]) = 
+#[C,B1,A]
+# merge is joining head of list that is not in TAIL
