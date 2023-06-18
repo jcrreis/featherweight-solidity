dd97d276e9ae6a082a46979d0fa8a611e0b25bce
João Reis <jcr.reis@campus.fct.unl.pt>
2023-06-18 15:41:59 +0100

    ...

--- contracts/openzeppelin/Game.sol
+++ contracts/openzeppelin/Game.sol
@@ -2,7 +2,7 @@ import "./NFTStorage.sol";
 
 contract Game {
 
-    mapping(address<NFTStorage> => NFTStorage) stores;
+    mapping(address => NFTStorage) stores;
 
 
     constructor() {
@@ -13,8 +13,12 @@ contract Game {
 
     }
 
-    function addStore() {
+    function createStore() {
         NFTStorage store = new NFTStorage{value: 0}();
         this.stores = (this.stores[address(store)] = store);
+    }
+
+    function addExternalStore(address<NFTStorage> store) {
+        this.stores = (this.stores[store] = NFTStorage(store));
     }
 }
